8844b2f0557bbf98bcec987b42baba8056436dfa
Sam Anderson <samel.2710@gmail.com>
2018-04-22 11:53:22 +0100

    Refactor Express App, add initial OAuth, connect Mongo

---
+++ server/config/keys-example.js
@@ -0,0 +1,5 @@
+module.exports = {
+    googleClientID: 'MYGOOGLECLIENTID',
+    googleClientSecret: 'MYGOOGLECLIENTSECRET',
+    mongoURI: 'MONGOURI'
+}
--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
-
-app.get('/', (req, res) => {
-    res.send({hi: 'there'});
-});
+require('./routes/auth-routes')(app)
 
 app.listen(PORT);
