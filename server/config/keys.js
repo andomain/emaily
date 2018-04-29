cca4d7009529c5881a9698dcf72a0dac3d26bb28
Sam Anderson <sam@andomain.co.uk>
2018-04-29 13:43:31 +0100

    Add debug to keys

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,6 +1,8 @@
 // keys.js - figure which credes to return
 if (process.env.NODE_ENV === 'production') {
+    console.log('Prod');
     module.exports = require('./prod');
 } else {
+    console.log('Dev');
     module.exports = require('./dev');
 }
