3c54700a250b68443cae4c337c987b3a6842f15f
Sam Anderson <sam@andomain.co.uk>
2018-05-15 22:38:13 +0100

    Further debug

--- server/config/keys.js
+++ server/config/keys.js
@@ -2,6 +2,7 @@ const prod = require('./prod');
 const dev = require('./dev');
 
 // keys.js - figure which credes to return
+console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'production') {
     module.exports = prod;
 } else {
