c7410f0de14e8a7accf2fae3a3bf0d1693096ce2
Sam Anderson <sam@andomain.co.uk>
2018-05-15 22:42:28 +0100

    Fix imports

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,10 +1,9 @@
-const prod = require('./prod');
-const dev = require('./dev');
+// const prod = require('./prod');
+// const dev = require('./dev');
 
 // keys.js - figure which credes to return
-console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'production') {
-    module.exports = prod;
+    module.exports = require('./prod');
 } else {
-    module.exports = dev;
+    module.exports = require('./dev');;
 }
--- server/services/passport.js
+++ server/services/passport.js
@@ -16,8 +16,6 @@ passport.deserializeUser((id, done) => {
         .then(user => done(null, user));
 });
 
-console.log(keys);
-
 passport.use(new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
