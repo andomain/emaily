3ab07316bea818c3cc498708c29e2c1cea4ac821
Sam Anderson <sam@andomain.co.uk>
2018-05-15 23:58:58 +0100

    Move app out of server dir

---
+++ config/dev.js
@@ -0,0 +1,9 @@
+// dev.js - don't commit this!
+module.exports = {
+    googleClientID: '1068212851682-tjc5sbelvguppk4qr0cboph4bomsvkpt.apps.googleusercontent.com',
+    googleClientSecret: 'zMKm0PaaTYbCvRVmgbhO9lYt',
+    mongoURI: 'mongodb://andomain:Andowipe89@ds151259.mlab.com:51259/emaily-andomain-dev',
+    cookieKey: 'hjfsjgnskfjgblksfjgblskf',
+    stripePublishableKey: 'pk_test_O933btRqEVfEXEnEPTjw1u74',
+    stripeSecretKey: 'sk_test_3rrgG8U05HuhxzRLLzEoKOsc',
+};
