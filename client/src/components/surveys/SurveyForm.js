c1ac2f770179890e5651eafc4ef180ccffc50e33
Sam Anderson <sam@andomain.co.uk>
2018-06-09 20:22:45 +0100

    More lint

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -12,26 +12,26 @@ class SurveyForm extends Component {
     renderFields() {
         return _.map(FIELDS, ({ label, name }) => (
             <Field key={`key-${name}`} component={SurveyField} type="text" label={label} name={name} />
-        ))
-    };
+        ));
+    }
 
     render() {
         return (
             <div>
-                <form onSubmit={ this.props.handleSubmit(this.props.onSurveySubmit)}>
-                { this.renderFields() }
-                <Link to="/surveys" className="red btn-flat white-text">
-                    Cancel
-                </Link>
-                <button className="teal btn-flat right white-text" type="submit">
-                    Next
-                    <i className="material-icons right">done</i>
-                </button>
+                <form onSubmit={this.props.handleSubmit(this.props.onSurveySubmit)}>
+                    { this.renderFields() }
+                    <Link to="/surveys" className="red btn-flat white-text">
+                        Cancel
+                    </Link>
+                    <button className="teal btn-flat right white-text" type="submit">
+                        Next
+                        <i className="material-icons right">done</i>
+                    </button>
                 </form>
             </div>
         );
     }
-};
+}
 
 function validate(values) {
     const errors = {};
